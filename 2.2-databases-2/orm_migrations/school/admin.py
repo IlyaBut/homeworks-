c6ba9eae535215bd5d542ad34ad2aacf8a8315b2
Ilya Butenko <Forgithubtyy@gmail.com>
2025-05-01 20:36:00 +0300

    Initial commit

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, SchoolStudentTeachers
 
 
 @admin.register(Student)
@@ -10,4 +10,8 @@ class StudentAdmin(admin.ModelAdmin):
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SchoolStudentTeachers)
+class SchoolStudentTeachersAdmin(admin.ModelAdmin):
     pass
