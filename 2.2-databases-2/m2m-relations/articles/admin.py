39b52825a0b50ec477ffb3d6e43361998049786a
Ilya Butenko <Forgithubtyy@gmail.com>
2025-05-02 16:10:41 +0300

    Task2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 
-from .models import Article
+from articles.models import Article,Tag, Scope
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
+
+class RelationshipInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') is True:
+                count +=1
+            if count > 1:
+                raise ValidationError("is_main должен быть только один тег")
+            elif count == 0:
+                raise ValidationError("Назначте is_main")
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 8
+    formset = RelationshipInlineFormSet
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
+
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+# Создаем таблицу тегов.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название тега")
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
 
@@ -7,6 +17,8 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    # Добавляем отношение М2м.
+    tags = models.ManyToManyField(Tag, related_name="article", through ="Scope")
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +26,12 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# Создаем промежуточную таблицу. Таблица-связка Scope
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="scopes")
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="scopes")
+    is_main = models.BooleanField(default = False, verbose_name="основной тэг")
+
+
